c5688ca66369ea2d19ad24bcf280f3bd397dae82
ghana <ghana.chindukuri@gmail.com>
2020-10-25 17:02:38 +0530

    Initial and final commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Header from "./components/Header";
+import AddSong from "./components/AddSong";
+import SongList from "./components/SongList";
+import SongPlayer from "./components/SongPlayer";
+import { Grid, useMediaQuery, Hidden } from "@material-ui/core";
+import songReducer from "./reducer";
+
+export const SongContext = React.createContext({
+  song: {
+    id: "01cf923a-e11b-4a58-ad2f-81712cb57e2c",
+    title: "Rap God (Explicit) [Official Video]",
+    artist: "Eminem",
+    thumbnail: "http://img.youtube.com/vi/XbGs_qK2PQA/0.jpg",
+    url: "https://www.youtube.com/watch?v=XbGs_qK2PQA",
+    duration: 370
+  },
+  isPlaying: false
+})
+
+function App() {
+  const initialSongState = React.useContext(SongContext);
+  const [state, dispatch] = React.useReducer(songReducer, initialSongState);
+  const greaterThanSm = useMediaQuery(theme => theme.breakpoints.up("sm"));
+  const greaterThanMd = useMediaQuery(theme => theme.breakpoints.up("md"));
+
+  return (
+    <SongContext.Provider value={{ state, dispatch }}>
+      <Hidden only="xs">
+        <Header />
+      </Hidden>
+      <Grid container spacing={3}>
+        <Grid
+          style={{
+            paddingTop: greaterThanSm ? 80 : 10
+          }}
+          item
+          xs={12}
+          md={7}
+        >
+          <AddSong />
+          <SongList />
+        </Grid>
+        <Grid
+          style={
+            greaterThanMd
+              ? {
+                  position: "fixed",
+                  width: "100%",
+                  right: 0,
+                  top: 70
+                }
+              : {
+                  position: "fixed",
+                  width: "100%",
+                  left: 0,
+                  bottom: 0
+                }
+          }
+          item
+          xs={12}
+          md={5}
+        >
+          <SongPlayer />
+        </Grid>
+      </Grid>
+    </SongContext.Provider>
+  );
+}
+
+export default App;
